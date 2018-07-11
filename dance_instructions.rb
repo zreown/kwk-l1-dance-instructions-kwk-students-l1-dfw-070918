fe9caee37310419d0bd313d39a550a80e75fa16b
zreown <zoe.reown@gmail.com>
2018-07-11 01:44:50 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,26 @@
 
 # Code your methods below
+def lower_body
+  puts "Put your right foot in"
+  puts "Take your right foot out"
+  puts "Put your right foot in"
+  puts "Shake your right foot"
+  puts "Turn around"
+  puts "Put your left foot in"
+  puts "Take your left foot out"
+  puts "Put your left foot in"
+  puts "Shake your left leg"
+  puts "Turn around"
+end
+def upper_body
+  puts "Put your right hand in"
+  puts "take your right hand out"
+  puts "Put your right hand in"
+  puts "shake your right hand"
+  puts "Turn around"
+  puts "put your left hand in"
+  puts "take your left hand out"
+  puts "put your left hand in"
+  puts "shake your left hand"
+  puts "turn around"
+end
